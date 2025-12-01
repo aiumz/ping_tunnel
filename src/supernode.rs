54e88383380999c7f876f86e694313b640d5929d
aiumz <352784769@qq.com>
2025-12-01 16:44:24 +0800

    chore: add supernode default config

--- src/supernode.rs
+++ src/supernode.rs
@@ -5,17 +5,15 @@ use std::env;
 async fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 5 {
-        eprintln!(
-            "Usage: {} <quic_bind_addr:port> <tcp_bind_addr:port> <cert_path> <key_path>",
-            args[0]
-        );
-        std::process::exit(1);
+    let mut quic_bind_addr = "0.0.0.0:4433".to_string();
+    let mut tcp_bind_addr = "0.0.0.0:4432".to_string();
+    let mut cert_path = "./cert/cert.pem".to_string();
+    let mut key_path = "./cert/key.pem".to_string();
+    if args.len() == 5 {
+        quic_bind_addr = args[1].clone();
+        tcp_bind_addr = args[2].clone();
+        cert_path = args[3].clone();
+        key_path = args[4].clone();
     }
-
-    let quic_bind_addr = args[1].clone();
-    let tcp_bind_addr = args[2].clone();
-    let cert_path = args[3].clone();
-    let key_path = args[4].clone();
     start_server(quic_bind_addr, tcp_bind_addr, cert_path, key_path).await
 }
